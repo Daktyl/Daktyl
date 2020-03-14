bb2dc0b05ec3864e185514a30e1e9dbea45f8915
KernelError <villainkernelerror@gmail.com>
2020-03-14 16:03:23 +0100

    Add some basic methods to IWebSocket

--- Daktyl.Core/WebSockets/IWebSocket.cs
+++ Daktyl.Core/WebSockets/IWebSocket.cs
@@ -1,7 +1,13 @@
-﻿namespace Daktyl.Core.WebSockets
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Daktyl.Core.WebSockets
 {
-	public interface IWebSocket
+	public interface IWebSocket : IAsyncDisposable
 	{
-		
+		Task Start(string endpoint);
+		Task Stop();
+		Task Send(ArraySegment<byte> data);
+		Task<ArraySegment<byte>> Receive();
 	}
 }
