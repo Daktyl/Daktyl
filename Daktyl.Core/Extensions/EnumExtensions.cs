1df78d562426f3bb245abed1cc49529ebedf8b40
KernelError <villainkernelerror@gmail.com>
2020-03-15 14:08:42 +0100

    Add extension method HasFlagFast to PermissionFlags

---
+++ Daktyl.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Daktyl.Core.Discord;
+
+namespace Daktyl.Core.Extensions
+{
+	public static class EnumExtensions
+	{
+		public static bool HasFlagFast(this PermissionFlags value, PermissionFlags flag)
+		{
+			return (value & flag) == flag;
+		}
+	}
+}
